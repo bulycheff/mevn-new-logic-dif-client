cde8f4fecb195fa8d76f71cb70bb395dc63d4123
Sergey Bulychev <bulycheff@ya.ru>
2021-11-22 06:49:36 +0500

    crm k9 change name

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export default {
-  // 'api_uri': 'http://localhost:5959/api/v1',
-  'api_uri': 'http://185.17.3.246:5555/api/v1',
+  'api_uri': 'http://localhost:5959/api/v1',
+  // 'api_uri': 'http://185.17.3.246:5555/api/v1',
 }
