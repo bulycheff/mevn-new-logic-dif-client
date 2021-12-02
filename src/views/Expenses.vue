abaf44f99c1b398841b8f17acea13b2295e5baf4
Sergey Bulychev <bulycheff@ya.ru>
2021-12-02 18:10:01 +0500

    added Loader to Expensece / fixed Console errors

--- src/views/Expenses.vue
+++ src/views/Expenses.vue
@@ -1,73 +1,87 @@
 <template>
 
-  <ul class="collapsible">
-    <li>
-      <div class="collapsible-header">
-        <i class="material-icons-sharp icon-font">paid</i>
-        <span>Внесение трат</span>
-      </div>
-      <div class="card-content collapsible-body">
-        <table>
-          <thead>
-          <tr>
-            <th></th>
-            <th>Дата</th>
-            <th>Категория трат</th>
-            <th>Сумма</th>
-            <th></th>
-          </tr>
-          </thead>
+  <Loader v-if="isLoading"/>
 
-          <tbody>
-          <tr v-for="item in expense_list" :key="item._id">
-            <td>#</td>
-            <td>{{ dateFilter(item.opened_at, 'datetime') }}</td>
-            <td>{{ item.category_name }}</td>
-            <td>{{ item.value }} ₽</td>
-            <td>
-              <span @click.stop="deleteItemById(item._id)" class="material-icons-round link">delete_outline</span>
-            </td>
-          </tr>
+  <div v-else-if="!isLoading" class="content">
+    <ul class="collapsible">
+      <li>
+        <div class="collapsible-header">
+          <i class="material-icons-sharp icon-font">paid</i>
+          <span>Внесение трат</span>
+        </div>
+        <div class="card-content collapsible-body">
+          <table>
+            <thead>
+            <tr>
+              <th></th>
+              <th>Дата</th>
+              <th>Категория трат</th>
+              <th>Сумма</th>
+              <th></th>
+            </tr>
+            </thead>
 
-          </tbody>
-        </table>
-        <br>
-        <i class="material-icons small link">add</i>
-        <ExpenseAdd/>
-      </div>
-    </li>
-  </ul>
+            <tbody>
+            <tr v-for="item in expense_list" :key="item._id">
+              <td>#</td>
+              <td>{{ dateFilter(item.opened_at, 'datetime') }}</td>
+              <td>{{ item.category_name }}</td>
+              <td>{{ item.value }} ₽</td>
+              <td>
+                <span @click.stop="deleteItemById(item._id)" class="material-icons-round link">delete_outline</span>
+              </td>
+            </tr>
+
+            </tbody>
+          </table>
+          <br>
+          <i class="material-icons small link">add</i>
+          <ExpenseAdd/>
+        </div>
+      </li>
+    </ul>
+  </div>
+
 </template>
 
 <script>
-import { computed, onBeforeMount, onMounted } from 'vue'
+import { computed, onBeforeMount, onUpdated, ref } from 'vue'
 import { useStore } from 'vuex'
 import { dateFilter } from '@/utils'
 import ExpenseAdd from '@/components/ExpenseAdd'
 import M from 'materialize-css'
+import Loader from '@/components/Loader'
 
 export default {
-  components: { ExpenseAdd },
+  components: { ExpenseAdd, Loader },
   setup() {
     const store = useStore()
+    const isLoading = ref(true)
+    const category_list = ref(computed(() => store.getters.CAT_itemList))
+    let expense_list = ref()
+
     onBeforeMount(async () => {
       await store.dispatch('EXPENSE_FETCH_ALL')
       await store.dispatch('CAT_FetchAll', { apiPath: 'base/cat' })
-    })
-    const category_list = computed(() => store.getters.CAT_itemList)
-    const expense_list = computed(() => {
-      return store.getters.expenseList.map(item => {
-        let obj = new Object(item)
-        obj.category_name = category_list.value.filter(cat_item => cat_item._id === item.category)[0].name
-        return obj
+      expense_list = computed(() => {
+        return store.getters.expenseList.map(item => {
+          let obj = new Object(item)
+          obj.category_name = category_list.value.filter(cat_item => cat_item._id === item.category)[0].name
+          return obj
+        })
       })
+      isLoading.value = false
     })
 
-    onMounted(()=>{
-      M.AutoInit()
+
+    let instance = ref()
+    let elems = ref()
+    onUpdated(()=> {
+      elems.value = document.querySelectorAll('.collapsible')
+      M.Collapsible.init(elems.value)
     })
 
-    const deleteItemById = async (id) => {
+    async function deleteItemById(id) {
       await store.dispatch('EXPENSE_DELETE_BY_ID', id)
     }
 
@@ -75,7 +89,10 @@ export default {
       expense_list,
       dateFilter,
       category_list,
-      deleteItemById
+      deleteItemById,
+      isLoading,
+      instance,
+      elems
     }
   }
 }
