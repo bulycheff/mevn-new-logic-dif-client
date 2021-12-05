dd10372c2f3fd1158d625a71281e30165bbc769d
Ubuntu CRM-K9 ROOT <bulycheff@ya.ru>
2021-12-04 21:48:50 -0500

    edited src/config/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
 export default {
-  'api_uri': 'http://localhost:5959/api/v1',
+  'api_uri': 'https://localhost:8902/api/v1',
   // 'api_uri': 'http://185.17.3.246:5555/api/v1',
   // 'api_uri': 'https://crm-k9.ru:8901/api/v1',
+// 'api_uri': 'https://localhost:8902/api/v1',
+
 }
