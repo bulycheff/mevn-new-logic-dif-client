4724b27902e8705f77e273a91f90430a8fbafb59
Sergey Bulychev <bulycheff@ya.ru>
2021-12-06 03:25:22 +0500

    set correct uri for store/history api requet

--- src/store/modules/history.js
+++ src/store/modules/history.js
@@ -1,14 +1,15 @@
 import axios from 'axios'
+import config from '@/config'
 
 export default {
   actions: {
     DAYS_HISTORY_FETCH: async ({ commit }) => {
-      const response = await axios.get('http://localhost:5959/api/v1/purchase/aggregate')
+      const response = await axios.get(`${config.api_uri}/purchase/aggregate`)
       const data = response.data
       commit('DaysHistory_SET', data)
     },
     DAYS_HISTORY_NEW_FETCH: async ({ commit }, [date1, date2]) => {
-      const response = await axios.get(`http://localhost:5959/api/v1/history/days?from=${date1}&to=${date2}`)
+      const response = await axios.get(`${config.api_uri}/history/days?from=${date1}&to=${date2}`)
       const data = response.data
       commit('DaysHistoryNew_SET', data)
     }
